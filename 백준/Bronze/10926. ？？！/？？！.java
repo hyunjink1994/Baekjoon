3677b8f9b6df98b642c62bed68f7a5e2ca237c29
hyunjink1994 <124650624+hyunjink1994@users.noreply.github.com>
2023-09-11 17:45:16 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] Args){
+		Scanner sc = new Scanner(System.in);
+
+		String x = sc.next();
+
+		System.out.printf("%s??!\n",x);
+	}
+}
