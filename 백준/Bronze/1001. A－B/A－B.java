c749cac4c904e7a5e8b494c4cc2d78e03ba6788e
hyunjink1994 <124650624+hyunjink1994@users.noreply.github.com>
2023-09-11 17:35:59 +0900

    [Bronze V] Title: A-B, Time: 212 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] Args){
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+
+		System.out.println(x-y);
+	}
+}
