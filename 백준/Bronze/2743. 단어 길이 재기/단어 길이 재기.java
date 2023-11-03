448aa9981e3aa4e4391252d9e92f70c61971412f
hyunjink1994 <124650624+hyunjink1994@users.noreply.github.com>
2023-11-03 13:24:40 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 132 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuffer sb = new StringBuffer();
+
+		String str = br.readLine();
+		System.out.println(str.length());
+	}
+}
