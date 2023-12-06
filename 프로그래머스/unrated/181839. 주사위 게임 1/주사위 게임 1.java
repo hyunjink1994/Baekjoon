1f090b006168931cdce26b96af1a7f8e5090a3b0
hyunjink1994 <124650624+hyunjink1994@users.noreply.github.com>
2023-12-06 21:05:20 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.10 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,17 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if(a%2 == 1 && b%2 == 1){
+            return (int) (Math.pow(a,2)+ Math.pow(b,2));
+        }
+        else if (a%2 == 0 && b%2 ==0){
+            return (int) (Math.abs(a-b));
+        }
+        else{
+            return 2*(a+b);
+        }
+    }
+}
