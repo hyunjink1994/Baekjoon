39d2571ea02572e8d9eb46184c0b253aaf2780b0
hyunjink1994 <124650624+hyunjink1994@users.noreply.github.com>
2024-06-28 18:47:16 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 172 ms, Memory: 30,692 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,89 +1,63 @@
+import java.util.*;
 import java.io.*;
-import java.lang.management.MemoryUsage;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.lang.Math;
+import java.text.DecimalFormat;
 
-public class Solution {
+public class Solution{
 
-    public static void printMemotyUsage(){
-        Runtime runtime = Runtime.getRuntime();
-        System.out.println("Usage Memory : " +(runtime.totalMemory() - runtime.freeMemory())/1024/1024 + " mb");
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        // input
+        for(int cn = 0; cn < 10; cn++){
+            st = new StringTokenizer(br.readLine());
+            int case_num = Integer.parseInt(st.nextToken());
+            int[][] arr = new int[100][100];
+            for(int i=0; i<100; i++){
+                st = new StringTokenizer(br.readLine());
+                for(int j=0;j<100;j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            System.out.printf("#%d %d\n", case_num, SolutionAnswer(arr));
+        }
     }
 
-    public static boolean constraint(ArrayList<ArrayList<Integer>> arr){
-        boolean answer = false;
+    public static int SolutionAnswer(int[][] arr){
 
-        if(arr.size() == 100 && arr.get(0).size() == 100){
-            answer = true;
+        int answer = 0;
+
+        for(int i=0; i<100; i++){
+            int sumX = 0;
+            int sumY = 0;
+            for(int j=0;j<100;j++){
+                sumX += arr[i][j];
+                sumY += arr[j][i];
+            }
+            if(answer <= sumX || answer < sumY){
+                if(sumX < sumY){
+                    answer = sumY;
+                }else{
+                    answer = sumX;
+                }
+            }
+        }
+        int sumA =0;
+        int sumB =0;
+        for(int i=0;i<100;i++){
+            sumA+=arr[99-i][i];
+            sumB+=arr[i][i];
+        }
+        if(answer <= sumA || answer < sumB){
+            if(sumA < sumB){
+                answer = sumB;
+            }else{
+                answer = sumA;
+            }
         }
 
         return answer;
     }
-
-    public static void main(String[] args) throws IOException {
-        long startTime = System.currentTimeMillis();
-//        System.setIn(new FileInputStream("SWEA/src/input.txt"));
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer stringTokenizer;
-
-        int T = 10;
-
-        for(int tc = 1; tc<= T; tc++){
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int caseNumber = Integer.parseInt(stringTokenizer.nextToken());
-
-            ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-
-            for (int i = 0; i < 100; i++) {
-                ArrayList<Integer> row = new ArrayList<>();
-                stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-                for (int j = 0; j < 100; j++) {
-                    row.add(Integer.parseInt(stringTokenizer.nextToken()));
-                }
-                arr.add(row);
-            }
-
-            System.out.printf("#%d %d\n", tc, answer(arr));
-        }
-
-        long endTime = System.currentTimeMillis();
-//        System.out.printf("time : " + (endTime - startTime) + " ms\n");
-//        printMemotyUsage();
-    }
-
-    public static int answer(ArrayList<ArrayList<Integer>> arr){
-        int left = 0;
-        int right = 0;
-        ArrayList<Integer> sumArr = new ArrayList<>();
-
-        for (int i = 0; i < arr.size(); i++) {
-            left+=arr.get(i).get(i);
-            right+=arr.get(i).get(99-i);
-            int row = 0;
-            int col = 0;
-            for (int j = 0; j < arr.size() ; j++) {
-                row += arr.get(i).get(j);
-                col += arr.get(j).get(i);
-            }
-            sumArr.add(row);
-            sumArr.add(col);
-        }
-        int max = 0;
-        for (int i = 0; i < sumArr.size(); i++) {
-            if(max < sumArr.get(i)){
-                max = sumArr.get(i);
-            }
-        }
-
-        if(max < left){
-            max = left;
-        } else if (max < right) {
-            max = right;
-        }
-
-        return max;
-    }
-
 }
