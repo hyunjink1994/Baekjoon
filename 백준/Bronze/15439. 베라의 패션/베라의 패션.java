58e35dedc1a644c735f6cff0e40bec09cf09ab01
hyunjink1994 <124650624+hyunjink1994@users.noreply.github.com>
2024-06-28 22:41:30 +0900

    [Bronze IV] Title: 베라의 패션, Time: 108 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+        
+        System.out.printf("%d", num*(num-1));
+    }
+}
