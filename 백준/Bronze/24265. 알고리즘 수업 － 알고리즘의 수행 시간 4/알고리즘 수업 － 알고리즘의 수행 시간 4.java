241d2ca29ed3eeb3ddaf999f5c9b9e4b87abd76a
hyunjink1994 <124650624+hyunjink1994@users.noreply.github.com>
2024-07-24 17:02:55 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 124 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int input = Integer.parseInt(st.nextToken());
+
+        System.out.println((long) input * (input - 1) /2);
+        System.out.println(2);
+
+
+    }
+}
