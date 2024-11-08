a705e6c8d4e3e494ede07095aa71cc592e334961
hyunjink1994 <124650624+hyunjink1994@users.noreply.github.com>
2024-11-08 09:18:35 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 7.60 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    public int solution(String myString, String pat) {
+        
+        if(myString.toUpperCase().contains(pat.toUpperCase())){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
