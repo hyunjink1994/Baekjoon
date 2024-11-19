e917653aefe08b858a8ede4cfb9d465252fe67c5
hyunjink1994 <124650624+hyunjink1994@users.noreply.github.com>
2024-11-19 09:09:25 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.06 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<str_list.length; i++){
+            if(!str_list[i].contains(ex)){
+                sb.append(str_list[i]);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
